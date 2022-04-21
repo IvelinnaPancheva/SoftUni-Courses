32710c5cb76abcaf374d3cf58d6b8303fb18fb2e
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-04-21 11:58:46 +0300

    Add files via upload

---
+++ programming_basics/drawing_figures/rhombus_of_stars.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for row in range(1, n+1):
+    print(" " * (n - row) + "*" + " *" * (row - 1))
+
+for row in range(n - 1, 0, -1):
+    print(" " * (n - row) + "*" + " *" * (row - 1))
+
