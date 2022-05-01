76ddc13c7ce8ebe637f9257ee9098a284f4a05a0
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-05-01 11:36:39 +0300

    Add files via upload

---
+++ programming_fundamentals/01-Basic-Syntax-Conditional-Statements-and-Loops-Exercise/double_char.py
@@ -0,0 +1,10 @@
+text = input()
+new_text = ""
+
+for i in range(len(text)):
+    new_text += text[i] * 2
+
+print(new_text)
+
+
+
---
+++ programming_fundamentals/01-Basic-Syntax-Conditional-Statements-and-Loops-Exercise/leonardo_dicaprio_oscars.py
@@ -0,0 +1,10 @@
+integer = int(input())
+
+if integer == 88:
+    print("Leo finally won the Oscar! Leo is happy")
+elif integer == 86:
+    print("Not even for Wolf of Wall Street?!")
+elif integer != 86 and integer < 88:
+    print("When will you give Leo an Oscar?")
+elif integer > 88:
+    print(f"Leo got one already!")
