f0b4e6b7a2a28c962a914c646ebe3557289715eb
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-05-02 18:44:42 +0300

    Add files via upload

---
+++ programming_fundamentals/01-Basic-Syntax-Conditional-Statements-and-Loops-More-Exercise/sum_of_a_beach-copy.py
@@ -0,0 +1,16 @@
+text = input()
+
+string = text.lower()
+
+word1 = "sand"
+word2 = "water"
+word3 = "fish"
+word4 = "sun"
+
+count1 = string.count(word1)
+count2 = string.count(word2)
+count3 = string.count(word3)
+count4 = string.count(word4)
+
+counter = count1 + count2 + count3 + count4
+print(counter)
---
+++ programming_fundamentals/01-Basic-Syntax-Conditional-Statements-and-Loops-More-Exercise/sum_of_a_beach.py
@@ -0,0 +1,11 @@
+text = input()
+
+string = text.lower()
+
+word1 = "sand"
+word2 = "water"
+word3 = "fish"
+word4 = "sun"
+
+counter = string.count(word1) + string.count(word2) + string.count(word3) + string.count(word4)
+print(counter)
