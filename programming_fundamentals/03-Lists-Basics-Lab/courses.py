4648c63044fcccb53746c5503227b3b0940ca6e2
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-05-06 10:51:08 +0300

    Add files via upload

---
+++ programming_fundamentals/03-Lists-Basics-Lab/courses.py
@@ -0,0 +1,8 @@
+n = int(input())
+list_of_courses = list()
+
+for i in range(n):
+    course = input()
+    list_of_courses.append(course)
+
+print(list_of_courses)
---
+++ programming_fundamentals/03-Lists-Basics-Lab/courses2.py
@@ -0,0 +1,7 @@
+n = int(input())
+list_of_courses = list()
+
+for i in range(n):
+    course = list_of_courses.append(input())
+
+print(list_of_courses)
