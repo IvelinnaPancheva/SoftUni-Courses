3b621b8b9a6f181e015534fcca495c44f3594a6f
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-05-10 22:39:43 +0300

    Add files via upload

---
+++ programming_fundamentals/03-Lists-Basics-More-Exercises/messaging.py
@@ -0,0 +1,25 @@
+sequence_of_numbers = input().split(" ")
+chars_string = list(input())
+sum_var = 0
+message = ""
+
+for num in sequence_of_numbers:
+    sum_var = 0
+    for i in range(len(num)):
+        sum_var += int(num[i])
+
+    if sum_var <= len(chars_string):
+        for j in range(len(chars_string)):
+            if j == sum_var:
+                message += chars_string[j]
+                chars_string.remove(chars_string[j])
+                break
+    else: # sumvar > len(chars_string)
+        sum_var = sum_var % len(chars_string)
+        for j in range(len(chars_string)):
+            if j == sum_var:
+                message += chars_string[j]
+                chars_string.remove(chars_string[j])
+                break
+
+print(message)
