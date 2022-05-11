143b183e1fc1084d40c7a12cf49a81e94c49588c
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-05-11 15:08:00 +0300

    Add files via upload

---
+++ programming_fundamentals/04-Functions-Lab/calculate_rectangle_area.py
@@ -0,0 +1,7 @@
+def calculate_rectangle_area(width, height):
+    return width * height
+
+
+w = int(input())
+h = int(input())
+print(calculate_rectangle_area(w, h))
---
+++ programming_fundamentals/04-Functions-Lab/calculate_rectangle_area_lambda.py
@@ -0,0 +1,4 @@
+w = int(input())
+h = int(input())
+calculate_rectangle_area = lambda width, height: width * height
+print(calculate_rectangle_area(w, h))
