aa3bb7c17417bf7bb654379bfa6bf243070436e2
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-05-18 22:02:13 +0300

    Add files via upload

---
+++ programming_fundamentals/05-Lists-Advanced-Exercise/which_are_in2.py
@@ -0,0 +1,10 @@
+first_sequence = input().split(", ")
+second_sequence = input().split(", ")
+substring_list = []
+
+for string in first_sequence:
+    for word in second_sequence:
+        if string in word and string not in substring_list:
+            substring_list.append(string)
+
+print(substring_list)
