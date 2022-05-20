22c9d4e91793b98ab4b8cf70dcee74757309bfbf
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-05-20 21:10:21 +0300

    Add files via upload

---
+++ programming_fundamentals/05-Lists-Advanced-Exercise/group_of_tens2.py
@@ -0,0 +1,19 @@
+def group_sorting_to_list(numbers):
+    for index in range(len(numbers)):
+        if numbers[index] <= ten_variable:
+            current = numbers[index]
+            output_list.append(current)
+    return output_list
+
+
+numbers_sequence = [int(x) for x in input().split(", ")]
+ten_variable = 10
+
+while len(numbers_sequence) > 0:
+    output_list = []
+    group_sorting_to_list(numbers_sequence)
+    for index in range(len(numbers_sequence) - 1, -1, -1):
+        if numbers_sequence[index] in output_list:
+            numbers_sequence.pop(index)
+    print(f"Group of {ten_variable}'s: {output_list}")
+    ten_variable += 10
