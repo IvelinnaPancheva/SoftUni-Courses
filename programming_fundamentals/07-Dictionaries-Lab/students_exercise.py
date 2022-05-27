b38d2079f8640b2b48099d1d3e109e8a5ed4ebe9
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-05-27 09:43:51 +0300

    Add files via upload

---
+++ programming_fundamentals/07-Dictionaries-Lab/students_exercise.py
@@ -0,0 +1,23 @@
+data = input().split(":")
+courses = dict()
+
+while len(data) != 1:
+    student_name = data[0]
+    id_num = int(data[1])
+    course_name = data[2]
+    if course_name not in courses:
+        courses[course_name] = {id_num: student_name}
+    courses[course_name][id_num] = student_name
+
+    data = input().split(":")
+
+searched_course = "".join(data)
+searched_course = searched_course.split("_")
+searched_course = " ".join(searched_course)
+
+for course_name in courses.keys():
+    if searched_course == course_name:
+        for student in courses[course_name]:
+            print(f"{courses[course_name][student]} - {student}")
+
+
