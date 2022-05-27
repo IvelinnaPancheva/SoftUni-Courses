ba33fa46760e7f51696fe6970cb5a760ae197970
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-05-27 09:45:25 +0300

    Add files via upload

---
+++ programming_fundamentals/07-Dictionaries-Lab/odd_occurance_exercise.py
@@ -0,0 +1,14 @@
+sequence = input().split(" ")
+chars_occurrence = dict()
+sequence = [char.lower() for char in sequence]
+for element in sequence:
+    count = 1
+
+    if element not in chars_occurrence:
+        chars_occurrence[element] = count
+    else:  # if chars exist as a key in the dictionary already with a value
+        chars_occurrence[element] += count
+
+for char in chars_occurrence.keys():
+    if chars_occurrence[char] % 2 != 0:
+        print(f"{char}", end=" ")
