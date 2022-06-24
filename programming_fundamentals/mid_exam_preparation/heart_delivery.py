ddbb77bf7de346a357e4a855bcd0c43aa7676aa2
IvelinnaPancheva <103529881+IvelinnaPancheva@users.noreply.github.com>
2022-06-24 21:19:08 +0300

    Add files via upload

---
+++ programming_fundamentals/mid_exam_preparation/heart_delivery.py
@@ -0,0 +1,35 @@
+houses = input().split("@") # houses even int hearts become a list of string values
+houses = list(map(int, houses)) # even nums from string become int
+command = input().split(" ") # command becomes a list with space for a separator
+starting_index = 0
+
+while command[0] != "Love!":
+    current_length = int(command[1])  # value of jump
+    if starting_index + current_length <= len(houses) - 1: # check if new index value is valid
+        house_index = starting_index + current_length
+
+        if houses[house_index] == 0:
+            print(f"Place {house_index} already had Valentine's day.")
+        elif houses[house_index] >= 2:
+            houses[house_index] -= 2
+            if houses[house_index] == 0:
+                print(f"Place {house_index} has Valentine's day.")
+        starting_index = house_index
+    else: # starting index + current length > last index
+        house_index = 0
+        starting_index = 0
+        if houses[house_index] == 0:
+            print(f"Place {house_index} already had Valentine's day.")
+        elif houses[house_index] >= 2:
+            houses[house_index] -= 2
+            if houses[house_index] == 0:
+                print(f"Place {house_index} has Valentine's day.")
+
+    command = input().split(" ")
+
+print(f"Cupid's last position was {house_index}.")
+if len(houses) == houses.count(0):
+    print("Mission was successful.")
+else:
+    failed_houses = len(houses) - houses.count(0)
+    print(f"Cupid has failed {failed_houses} places.")
